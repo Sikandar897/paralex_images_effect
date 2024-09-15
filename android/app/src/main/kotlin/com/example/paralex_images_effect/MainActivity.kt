11bea90c974b4824301dfbbd65b9ea5d2274c4d2
Sikandar897 <sikandarafridi6@gmail.com>
2024-09-15 18:52:44 +0500

    paraleximageEffext

---
+++ android/app/src/main/kotlin/com/example/paralex_images_effect/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.paralex_images_effect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Model/data.dart
@@ -0,0 +1,30 @@
+List<Map> pkplaces = [
+  {
+    'image': 'assets/hakwasbey.jpg',
+    'name': 'Hawks Bay',
+  },
+  {
+    'image': 'assets/hunza.jpg',
+    'name': 'Hunza Valley',
+  },
+  {
+    'image': 'assets/kumratvelly.jpg',
+    'name': 'Kumrat Valley',
+  },
+  {
+    'image': 'assets/mukshpuritop.jpg',
+    'name': 'Mukshpuri Peak',
+  },
+  {
+    'image': 'assets/neelumvalley.jpg',
+    'name': 'Neelum Valley',
+  },
+  {
+    'image': 'assets/saifulmalouk.jpg',
+    'name': 'Saif-ul-Muluk Lake',
+  },
+  {
+    'image': 'assets/upperkachura.jpg',
+    'name': 'Upper Kachura Lake',
+  },
+];
